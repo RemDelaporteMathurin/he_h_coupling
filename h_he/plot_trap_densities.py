2ab67338da43eb9b4e54953e3f7ba79494732e4b
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-09-05 17:00:07 +0200

    new colours

--- h_he/plot_trap_densities.py
+++ h_he/plot_trap_densities.py
@@ -20,7 +20,7 @@ elif tds_number == 5:
     density_3 = 1.5e-3
 
 
-labels = ['Trap 1', 'Trap 2', 'Trap 3']
+labels = ["Trap 1", "Trap 2", "Trap 3"]
 tds_2 = [2.2e-3, 1.8e-3, 2.0e-3]
 tds_5 = [3.8e-3, 3.1e-3, 1.5e-3]
 
@@ -28,8 +28,8 @@ x = np.arange(len(labels))  # the label locations
 width = 0.35  # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, tds_2, width, label='2nd TDS')
-rects2 = ax.bar(x + width/2, tds_5, width, label='5th TDS')
+rects1 = ax.bar(x - width / 2, tds_2, width, label="2nd TDS", color="tab:orange")
+rects2 = ax.bar(x + width / 2, tds_5, width, label="5th TDS", color="tab:green")
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 def latex_float(f):
@@ -40,18 +40,19 @@ def latex_float(f):
     else:
         return float_str
 
-ax.set_ylabel('Density (at.fr.)')
+
+ax.set_ylabel("Density (at.fr.)")
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
-plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0), useMathText=True)
+plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0), useMathText=True)
 
 ax.legend()
 
 ax.bar_label(rects1, padding=3, labels=["$" + latex_float(f) + "$" for f in tds_2])
 ax.bar_label(rects2, padding=3, labels=["$" + latex_float(f) + "$" for f in tds_5])
 ax = plt.gca()
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
 fig.tight_layout()
 
 plt.show()
